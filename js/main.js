8deba3b62c2ca204432a1325fa14a0fa75ec5cf0
Donald Felician <donaldfelician@gmail.com>
2016-10-20 22:23:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
-$(document).ready(function () {
-	
-	// display the navigation after scrolling
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 100) {
-			$(".navigation").fadeIn(200);	
-		} else{
-			$(".navigation").fadeOut(200);
-		}
-	});
+$(document).ready(function() {
 
-	$('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+            // Sets viewScroller
+            $('.mainbag').viewScroller({
+                useScrollbar: false,
+                changeWhenAnim: false
+            });
 
-
-});
+}); //end of ready
